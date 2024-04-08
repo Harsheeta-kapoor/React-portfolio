a97f9b38519d637d56f9b1f76a15282cb3fcb6d9
Harsheeta <harsheeta.kap@gmail.com>
2024-04-08 22:00:03 +0530

    hero section and experience added

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,18 @@
 // rather than passing a string value we passed a variable in curly braces
 // {styles.App} , this App is the actual class name that is difined in app.module.css
 import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
+      <Hero />
+      <About />
+      <Experience />
     </div>
   );
 }
